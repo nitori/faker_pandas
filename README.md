912ae8e66a4137a33deec5c92e0a082013337811
Lars Peter Søndergaard <l.soendergaard@lichtflut-medien.de>
2022-04-30 22:40:01 +0200

    Update PandasProvider
    - rename series generator to column generator
    - column generators now return iterators instead of callables
    - various code improvements to make adding new features easier
    - Update README.md

--- README.md
+++ README.md
@@ -8,23 +8,25 @@ from faker_pandas import PandasProvider
 fake = Faker()
 fake.add_provider(PandasProvider)
 
-series = fake.pandas_series_generator()
+colgen = fake.pandas_column_generator()
 
 df = fake.pandas_dataframe(
-    series.first_name('First Name'),
-    series.last_name('Last Name'),
-    series.pandas_int('Age', 18, 80),
-    rows=5
+    colgen.first_name('First Name', empty_value='', empty_ratio=.5),
+    colgen.last_name('Last Name'),
+    colgen.pandas_int('Age', 18, 80, empty_ratio=.2),
+    rows=7
 )
 
 print(df)
 ```
 Output:
 ```txt
-  First Name Last Name  Age
-1   Kimberly  Humphrey   63
-2     Leslie     White   55
-3    Melanie     Brown   20
-4   Jennifer      Diaz   23
-5      Eddie     Ellis   63
+  First Name Last Name   Age
+1             Lawrence  72.0
+2       Lisa  Holloway   NaN
+3              Edwards  31.0
+4     Steven   Johnson  69.0
+5                Smith  66.0
+6     Monica     Lynch   NaN
+7     Edward     Brown  20.0
 ```
--- faker_pandas/provider.py
+++ faker_pandas/provider.py
@@ -1,6 +1,6 @@
 from faker.providers import BaseProvider
 from faker.generator import random
-from functools import partial
+from functools import partial, wraps
 import pandas as pd
 
 _AUTO = object()
@@ -11,11 +11,9 @@ class PandasProvider(BaseProvider):
     def pandas_dataframe(
         self,
         *generators,
-        columns=None,
         rows=7,
         index=_AUTO
     ):
-
         if index is _AUTO:
             index = range(1, rows + 1)
 
@@ -23,69 +21,100 @@ class PandasProvider(BaseProvider):
         for _ in range(rows):
             row = []
             for generator in generators:
-                row.append(generator())
+                row.append(next(generator))
             row_list.append(row)
 
-        if columns is None:
-            columns = []
-            for generator in generators:
-                columns.append(generator.__name__)
-        else:
-            columns = list(columns)[:len(generators)]
-            for generator in generators[len(columns):]:
-                columns.append(generator.__name__)
+        columns = []
+        for generator in generators:
+            columns.append(generator.__name__)
 
         df = pd.DataFrame(row_list, index=index, columns=columns)
         return df
 
-    def pandas_series_generator(self):
-        return SeriesGenerator(self.generator)
+    def pandas_column_generator(self):
+        return ColumnGenerator(self.generator)
 
 
-class SeriesGenerator:
+def argumentarize(func):
+    @wraps(func)
+    def decorator(decorated_func=None, /, **kwargs):
+        if decorated_func is None:
+            # decorator was used with (...)
+            # so return the decorator again as partial
+            return partial(func, **kwargs)
+        if not callable(decorated_func):
+            raise TypeError(f"First argument of {func} must be callable. Use keyword arguments instead.")
+        return func(decorated_func, **kwargs)
+
+    return decorator
+
+
+@argumentarize
+def wrap_iterator(func, /, **deco_kwargs):
+    @wraps(func)
+    def _wrapper(self, name, *args, **kwargs):
+        if not hasattr(func, '__self__'):
+            bound_func = func.__get__(self, self.__class__)
+        else:
+            bound_func = func
+
+        passed_kwargs = {**deco_kwargs, **kwargs}
+        return ColumnIterator(name, bound_func, *args, **passed_kwargs)
+
+    return _wrapper
+
+
+class ColumnGenerator:
     def __init__(self, generator):
         self.generator = generator
 
     def __getattr__(self, item):
         func = getattr(self.generator, item)
+        return partial(wrap_iterator(func), self.generator)
 
-        def _gen(name):
-            return _SeriesCaller(name, func)
+    @wrap_iterator(empty_value=float('nan'))
+    def pandas_int(self, a, b):
+        return random.randint(a, b)
 
-        return _gen
-
-    def pandas_int(self, name, a, b, *, nan_ratio=0):
-        def _gen():
-            return random.choices(
-                [random.randint(a, b), float('nan')],
-                [1 - nan_ratio, nan_ratio],
-                k=1
-            )[0]
-
-        return _SeriesCaller(name, _gen)
-
-    def pandas_float(self, name, *, nan_ratio=0):
-        def _gen():
-            return random.choices(
-                [random.random(), float('nan')],
-                [1 - nan_ratio, nan_ratio],
-                k=1
-            )[0]
-
-        return _SeriesCaller(name, _gen)
+    @wrap_iterator(empty_value=float('nan'))
+    def pandas_float(self):
+        return random.random(0, 1)
 
 
-class _SeriesCaller:
+class ColumnIterator:
 
-    def __init__(self, name, func, *args, **kwargs):
+    def __new__(cls, *args, **kwargs):
+        obj = super().__new__(cls)
+        if 'rows' in kwargs:
+            obj.__init__(*args, **kwargs)
+            return obj.to_series()
+        return obj
+
+    def __init__(self, name, func, *args, rows=None, empty_value=None, empty_ratio=0.0, **kwargs):
         self.name = name
         self.func = func
         self.args = args
         self.kwargs = kwargs
 
-    def __call__(self, *args, **kwargs):
-        return self.func(*args, **kwargs)
+        self.rows = rows
+        self.empty_value = empty_value
+        self.empty_ratio = empty_ratio
 
     @property
     def __name__(self):
         return self.name
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if random.random() < self.empty_ratio:
+            return self.empty_value
+        return self.func(*self.args, **self.kwargs)
+
+    def to_series(self, *, rows=None):
+        if rows is None:
+            rows = self.rows
+        if rows is None:
+            raise ValueError('rows is not specified')
+        return pd.Series(item for item, _ in zip(self, range(rows)))
